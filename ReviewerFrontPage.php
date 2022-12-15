ae442e50eba054aee1fc0afc5f6c758608544c8c
hsutai <100201968+hsutai@users.noreply.github.com>
2022-12-15 21:20:30 +0800

    upload 待評閱判斷useriderid

--- ReviewerFrontPage.php
+++ ReviewerFrontPage.php
@@ -2,14 +2,22 @@
 <html lang="en">
 
 <?php
-                            
+session_start();
+$pageid = 2;
+if (!isset($_SESSION['identity']) OR $_SESSION['identity'] != $pageid ) {
+    echo "<script>alert('請以審稿者帳號登入');
+            location.assign('login.html');
+            </script>";
+}
+
 function pendingreview()
 {   require_once("cfg.php");
     require_once("sqlLink.php");
 
         $link =connect(DB_HOST,DB_USER,DB_PWD,DB_DATABASE)
         or die("無法開啟資料連接!<br/>");
-		$sql = "SELECT * FROM article WHERE state = '3'";
+		$sql = "SELECT * FROM article WHERE state = '3' and id=".$_SESSION['userid'];
+		
 							
 		if ($result = mysqli_query($link,$sql))
 			{
@@ -24,7 +32,7 @@ function review()
 
         $link =connect(DB_HOST,DB_USER,DB_PWD,DB_DATABASE)
         or die("無法開啟資料連接!<br/>");
-							$sql = "SELECT * FROM article WHERE state = '評閱中'";
+							$sql = "SELECT * FROM article WHERE state = '6'";
 							
 							if ($result = mysqli_query($link,$sql))
 							{
@@ -39,7 +47,7 @@ function Reviewafterrevision()
 
         $link =connect(DB_HOST,DB_USER,DB_PWD,DB_DATABASE)
         or die("無法開啟資料連接!<br/>");
-							$sql = "SELECT * FROM article WHERE state = '修改後評閱'"; 
+							$sql = "SELECT * FROM article WHERE state = '7'"; 
 							
 							if ($result = mysqli_query($link,$sql))
 							{
